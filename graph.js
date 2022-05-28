e6cae165a247d0efd4df3c09e43c03cf2b8f88b5
Naeem Ali <siyalnaeem1997@gmail.com>
2022-05-28 17:17:15 +0500

    implemented graph traversal - DFS and BFS

--- graph.js
+++ graph.js
@@ -37,14 +37,84 @@ class Graph {
             delete this.adjacencyList[name];
         }
     }
+
+    dfsR(vertex){
+        const nodes = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vert){
+            if(!vert) return null;
+            visited[vert] = true;
+            nodes.push(vert);
+            adjacencyList[vert].forEach(neighbour => {
+                if(!visited[neighbour]){
+                    return dfs(neighbour);
+                }
+            });
+        })(vertex);
+
+        return nodes;
+    }
+
+    dfsI(vertex){
+        const stack = [vertex];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[vertex] = true;
+
+        while(stack.length){
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(neighbour => {
+                if(!visited[neighbour]){
+                    visited[neighbour] = true;
+                    stack.push(neighbour);
+                }
+            });
+        }
+
+        return result;
+    }
+
+    bfs(vertex){
+        const queue = [vertex];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[vertex] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+            this.adjacencyList[currentVertex].forEach(sibling => {
+                if(!visited[sibling]){
+                    visited[sibling] = true;
+                    queue.push(sibling);
+                }
+            })
+        }
+        return result;
+    }
 }
 
 const graph = new Graph();
-graph.addVertex("tokyo");
-graph.addVertex("Pakistan");
-graph.addVertex("America");
-graph.addEdge("tokyo", "Pakistan")
-graph.addEdge("America", "Pakistan")
-// graph.removeEdge("America", "Pakistan")
-graph.removeVertex("America")
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addEdge("A", "B")
+graph.addEdge("A", "C")
+graph.addEdge("B", "D")
+graph.addEdge("C", "E")
+graph.addEdge("D", "E")
+graph.addEdge("D", "F")
+graph.addEdge("E", "F")
+
 console.log(graph.adjacencyList)
+console.log(graph.bfs("A"));
